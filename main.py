d17c2bae54f611fe191794ae03c3b32cbb1a58c3
Ivan <idloboda@gmail.com>
2024-11-22 19:43:47 -0600

    Added main ship, added rotation

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import pygame
 from constants import *
+from player import Player
+import player
 
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH} px")
     print(f"Screen height: {SCREEN_HEIGHT} px")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
-
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
